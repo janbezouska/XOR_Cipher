76b2c5565edfcdf55b7fc37c1f291a6a27cec40e
janbezouska <bezouska.jan.2018@skola.ssps.cz>
2021-03-14 12:44:57 +0100

    functional version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
+﻿using System.Windows;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace XOR_Cipher
 {
@@ -24,5 +13,71 @@ namespace XOR_Cipher
         {
             InitializeComponent();
         }
+
+        private void Cipher_Click(object sender, RoutedEventArgs e)
+        {
+            DeCipher(false);
+        }
+
+        private void Decipher_Click(object sender, RoutedEventArgs e)
+        {
+            DeCipher(true);
+        }
+        public void DeCipher(bool decipher)
+        {
+            string strContent = "";
+            string strKey = tbKey.Text;
+            string strOutput = "";
+            //List<int> backupList = new List<int>();
+            bool err = false;
+
+
+            if(!decipher)
+                strContent = new TextRange(rtbInput.Document.ContentStart, rtbInput.Document.ContentEnd).Text.Trim();
+            else
+                strContent = new TextRange(rtbOutput.Document.ContentStart, rtbOutput.Document.ContentEnd).Text.Replace("\r\n", "");
+
+
+            if (strContent == "")
+            {
+                tbErr.Foreground = Brushes.Red;
+                tbErr.Clear();
+                if (!decipher)
+                    tbErr.AppendText("NOTHING TO CIPHER");
+                else
+                    tbErr.AppendText("NOTHING TO DECIPHER");
+                err = true;
+            }
+            if (strKey == "")
+            {
+                if (err)
+                    tbErr.AppendText("\nNO KEY");
+                else
+                {
+                    tbErr.Foreground = Brushes.Red;
+                    tbErr.Clear();
+                    tbErr.AppendText("NO KEY");
+                }
+            }
+            else if (!err)
+            {
+                tbErr.Clear();
+
+                for (int i = 0; i < strContent.Length; i++)
+                    strOutput += (char)(strContent[i] ^ strKey[i % strKey.Length]);
+
+                if(!decipher)
+                {
+                    rtbOutput.Document.Blocks.Clear();
+                    rtbOutput.AppendText(strOutput);
+                }
+                else
+                {
+                    rtbInput.Document.Blocks.Clear();
+                    rtbInput.AppendText(strOutput);
+                }
+            }
+        }
     }
+
 }
