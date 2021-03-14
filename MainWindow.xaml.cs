5dbbadc96a04a45e6ff54da50e348ab65d13dafd
janbezouska <bezouska.jan.2018@skola.ssps.cz>
2021-03-14 13:09:52 +0100

    Přidání komentářů

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,63 +16,62 @@ namespace XOR_Cipher
 
         private void Cipher_Click(object sender, RoutedEventArgs e)
         {
-            DeCipher(false);
+            DeCipher(false); //spustí se metoda DeCipher s informací, že se jedná o šifrování
         }
 
         private void Decipher_Click(object sender, RoutedEventArgs e)
         {
-            DeCipher(true);
+            DeCipher(true); //spustí se metoda DeCipher s informací, že se jedná o dešifrování
         }
         public void DeCipher(bool decipher)
         {
-            string strContent = "";
-            string strKey = tbKey.Text;
-            string strOutput = "";
-            //List<int> backupList = new List<int>();
-            bool err = false;
+            string strContent = "";     //zde se uloží text, který budeme (de)šifrovat
+            string strKey = tbKey.Text; //klíč
+            string strOutput = "";      //zde se uloží text, který bude výsledkem (de)šifrování
+            bool err = false;           //informace o tom, zade se objevila určitá chyba
 
 
-            if(!decipher)
+            if(!decipher)   //pokud šifrujeme, uložíme text z RichTextBoxu pro šifrování
                 strContent = new TextRange(rtbInput.Document.ContentStart, rtbInput.Document.ContentEnd).Text.Trim();
-            else
+            else            //pokud dešifrujeme, uložíme text z RichTextBoxu pro dešifrování
                 strContent = new TextRange(rtbOutput.Document.ContentStart, rtbOutput.Document.ContentEnd).Text.Replace("\r\n", "");
 
 
-            if (strContent == "")
+            if (strContent == "")               //pokud je text prázdný, vypíšeme chybu
             {
-                tbErr.Foreground = Brushes.Red;
-                tbErr.Clear();
-                if (!decipher)
+                tbErr.Foreground = Brushes.Red; //červený text
+                tbErr.Clear();                  //smazání všeho na TextBoxu
+                if (!decipher)                  //vypsání chyby podle toho, zda šifrujeme, nebo dešifrujeme
                     tbErr.AppendText("NOTHING TO CIPHER");
                 else
                     tbErr.AppendText("NOTHING TO DECIPHER");
-                err = true;
+                err = true;                     //uložení informace o chybě
             }
-            if (strKey == "")
+            if (strKey == "")                       //pokud je klíč prázdný, vypíšeme chybu
             {
-                if (err)
+                if (err)                            //pokud nasatla předchozí chyba, připíšeme tuto pod ní
                     tbErr.AppendText("\nNO KEY");
-                else
+                else                                //pokud nenastala předchozí chyba, vypíšeme tuto
                 {
-                    tbErr.Foreground = Brushes.Red;
-                    tbErr.Clear();
-                    tbErr.AppendText("NO KEY");
+                    tbErr.Foreground = Brushes.Red; //červený text
+                    tbErr.Clear();                  //smazání všeho na TextBoxu
+                    tbErr.AppendText("NO KEY");     //vypsání chyby
                 }
             }
-            else if (!err)
+            else if (!err)      //pokud nenastala žádná chyba, (de)šifrujeme
             {
-                tbErr.Clear();
+                tbErr.Clear(); 
 
-                for (int i = 0; i < strContent.Length; i++)
-                    strOutput += (char)(strContent[i] ^ strKey[i % strKey.Length]);
-
+                for (int i = 0; i < strContent.Length; i++)     //procházíme jednotlivé znaky textu
+                    strOutput += (char)(strContent[i] ^ strKey[i % strKey.Length]); //provedeme XOR operaci na jednotlivé
+                                                                                    //znaky textu a klíče, viz readme
                 if(!decipher)
-                {
+                {              //pokud jsme šifrovali, vypíšeme výsledek do RichTextBoxu dešifrování
                     rtbOutput.Document.Blocks.Clear();
                     rtbOutput.AppendText(strOutput);
                 }
                 else
-                {
+                {              //pokud jsme dešifrovali, vypíšeme výsledek do RichTextBoxu šifrování
                     rtbInput.Document.Blocks.Clear();
                     rtbInput.AppendText(strOutput);
                 }
